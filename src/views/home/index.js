8cb6e3cfe289611fda72072653d39042b366b4ad
Shirjoy Bhuban <50661137+shirjoybhuban@users.noreply.github.com>
2024-09-05 13:49:29 +0600

    feat(virtualization) : infinite scroll added in post list

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -6,29 +6,59 @@ const HomeView = () => {
   const [loading, setLoading] = useState(true);
   // Loading state can be handle in locally or globally.
   const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState([]);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = () => {
+    setLoading(true);
     dispatch(
       postDispatcher.getPaginatedPosts(page, {
         success: (response) => {
           console.log("response", response);
-          setLoading(false);
+          setTimeout(() => {
+            setPosts((post) => [...post, ...response.posts]);
+            setLoading(false);
+          }, 5000);
         },
         error: (err) => {
           setLoading(false);
-          if(error.status == 500){
-            
+          if (error.status == 500) {
           }
           console.log("Error in fetching orders", err);
         },
       })
     );
-  }, []);
+  };
 
-  if (loading) return <h1>Loading</h1>;
-  return <>I will Feed here</>;
+  const handleScroll = () => {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) {
+      return;
+    }
+    fetchPosts();
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [loading]);
+
+  return (
+    <>
+      <ul>
+        {posts.map((item) => (
+          <li className="h-96 bg-slate-50" key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
+      <div>{loading && <h1>Loading</h1>}</div>
+    </>
+  );
 };
 
 export default HomeView;
